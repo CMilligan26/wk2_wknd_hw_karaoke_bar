f74c43170754cca3c76aec7508b3445bee8c22c6
Christopher Milligan <milliganjchristopher@gmail.com>
2018-10-26 16:00:59 +0100

    Starting Viewer

--- room.rb
+++ room.rb
@@ -1,10 +1,40 @@
 class Room
 
- attr_writer  
+  def initialize(karaoke_machine, bar, capacity)
+    @karaoke_machine = karaoke_machine
+    @bar = bar
+    @capacity = capacity
+    @current_guest_list = []
+  end
 
-def initialize(karaoke_machine, bar)
-  @karaoke_machine = karaoke_machine
-  @bar = bar
-end
+  def has_karaoke_machine
+    !@karaoke_machine.nil?
+  end
+
+  def has_bar
+    !@bar.nil?
+  end
+
+  def give_capacity
+    @capacity
+  end
+
+  def has_current_guest_list
+    !@current_guest_list.nil?
+  end
+
+  def enter_guest(guest)
+    return false if @current_guest_list.length >= give_capacity
+    starting_current_guest_list = @current_guest_list.length
+    @current_guest_list << guest
+    @current_guest_list.length > starting_current_guest_list
+  end
+
+  def remove_guest(guest)
+    return false if @current_guest_list.length <= 0
+    starting_current_guest_list = @current_guest_list.length
+    @current_guest_list.delete(guest)
+    @current_guest_list.length < starting_current_guest_list
+  end
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -12,7 +12,7 @@ class RoomTest < MiniTest::Test
     drink_1 = Drink.new("Coke", 1)
     drink_2 = Drink.new("Red Bull", 2)
     bar = Bar.new([drink_1, drink_2])
-    @room = Room.new(karaoke_machine, bar)
+    @room = Room.new(karaoke_machine, bar, 2)
   end
 
   #CREATED
@@ -20,5 +20,45 @@ class RoomTest < MiniTest::Test
   def test_room_is_created
     assert_equal(Room, @room.class)
   end
+
+  #KARAOKE MACHINE
+  def test_room_has_karaoke_machine
+    assert_equal(true, @room.has_karaoke_machine)
+  end
+
+  #BAR
+  def test_room_has_bar
+    assert_equal(true, @room.has_bar)
+  end
+
+  #CAPACITY
+  def test_room_give_capacity
+    assert_equal(2, @room.give_capacity)
+  end
+
+  #GUESTS IN ROOM
+  def test_room_has_guests_in_room_list
+    assert_equal(true, @room.has_current_guest_list)
+  end
+
+  def test_room_add_to_guests_in_room_list
+    assert_equal(true, @room.enter_guest("Test Guest"))
+  end
+
+  def test_room_add_to_guests_in_room_list__full_capacity
+    @room.enter_guest("Test Guest")
+    @room.enter_guest("Test Guest")
+    assert_equal(false, @room.enter_guest("Test Guest"))
+  end
+
+  def test_room_remove_from_guests_in_room_list
+    @room.enter_guest("Test Guest")
+    assert_equal(true, @room.remove_guest("Test Guest"))
+  end
+
+  def test_room_remove_from_guests_in_room_list__empty_room
+    assert_equal(false, @room.remove_guest("Test Guest"))
+  end
+
 
 end
