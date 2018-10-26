24be20230c3ea55de3208193bc3da11db6e9af62
Christopher Milligan <milliganjchristopher@gmail.com>
2018-10-26 13:03:42 +0100

    Guest and Front Desk complete

---
+++ specs/front_desk_spec.rb
@@ -0,0 +1,63 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../front_desk')
+# require_relative('../guest')
+
+class GuestTest < MiniTest::Test
+
+  def setup
+    @front_desk = FrontDesk.new
+  end
+
+  #CREATED
+
+  def test_front_desk_is_created
+    assert_equal(FrontDesk, @front_desk.class)
+  end
+
+  #STAFF MEMBER
+
+  def test_front_desk_has_staff_member
+    assert_equal("Zoe", @front_desk.provide_staff_member_name)
+  end
+
+  #ENTRY FEE
+
+  def test_front_desk_has_entry_fee
+    assert_equal(50, @front_desk.provide_entry_fee)
+  end
+
+  #TOTAL_CASH
+
+  def test_front_desk_has_total_cash
+    assert_equal(true, @front_desk.has_total_cash)
+  end
+
+  def test_front_desk_total_cash_can_accept_cash
+    assert_equal(true, @front_desk.accept_cash(50))
+  end
+
+  #GUESTS CHECKED IN LIST
+
+  def test_front_desk_has_guests_checked_in
+    assert_equal(true, @front_desk.has_guests_checked_in)
+  end
+
+  def test_front_desk_check_guest_in
+    assert_equal(true, @front_desk.check_in_guest("Test Guest"))
+  end
+
+  #GUESTS CHECKED OUT LIST
+
+  def test_front_desk_has_guests_checked_out
+    assert_equal(true, @front_desk.has_guests_checked_out)
+  end
+
+  def test_front_desk_check_out_guest
+    @front_desk.check_in_guest("Test Guest")
+    assert_equal(true, @front_desk.remove_from_checked_in_list("Test Guest"))
+    assert_equal(true, @front_desk.add_to_checked_out_list("Test Guest"))
+    assert_equal("Guest Checked Out", @front_desk.check_out_guest("Test Guest"))
+  end
+
+end
---
+++ specs/guest_spec.rb
@@ -0,0 +1,55 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../guest')
+
+class GuestTest < MiniTest::Test
+
+  def setup
+    @guest = Guest.new("Chris", "Baby Come Back")
+  end
+
+  #CREATED
+
+  def test_guest_is_created
+    assert_equal(Guest, @guest.class)
+  end
+
+  #NAME
+
+  def test_guest_has_name
+    assert_equal("Chris", @guest.provide_name)
+  end
+
+  #WALLET
+
+  def test_guest_has_wallet
+    assert_equal(true, @guest.has_wallet)
+  end
+
+  def test_guest_can_afford_price__true
+    assert_equal(true, @guest.can_afford(30))
+  end
+
+  def test_guest_can_afford_price__false
+    assert_equal(false, @guest.can_afford(150))
+  end
+
+  def test_guest_can_spend_money__true
+    assert_equal(60, @guest.spend_money(60))
+  end
+
+  def test_guest_can_spend_money__false
+    assert_equal(0, @guest.spend_money(150))
+  end
+
+  #FAVOURITE SONG
+
+  def test_guest_has_favourite_song
+    assert_equal(true, @guest.has_favourite_song)
+  end
+
+  def test_guest_woops_at_favourite_song
+    assert_equal("Woop!", @guest.woop_at_favourite_song("Baby Come Back") )
+  end
+
+end
